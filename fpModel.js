2d8457919318450fc6c4d4b8f05a947237c51e07
Brian Kim <hk2473@nyu.edu>
2021-11-21 17:18:16 -0500

    turn all array of JSON objects to JSON object with appropriate key value pairs

--- fpModel.js
+++ fpModel.js
@@ -37,8 +37,51 @@ const mongoose = require('mongoose');
 
 const Fp = mongoose.Schema({
     date : {type : Date, default : Date.now},
-    extensions : [Object]
+    extensions : Object
 })
 
 const FpModel = mongoose.model('fps', Fp);
 module.exports = FpModel;
+
+/*
+desired document
+
+{
+    "_id":"619abb233f43f2a241565384",
+    "extensions": {
+        "leechblock" : {
+            "11" : {
+                "background-color" : {
+                    "baselineValue": "rgba(0, 0, 0, 0)",
+                    "triggerValue": "rgb(255, 255, 255)"
+                },
+                "border-block-end-color" : {
+                    "baselineValue": "rgb(232, 230, 227)",
+                    "triggerValue": "rgb(0, 0, 0)"
+                }
+            },
+            "13" : {
+                "background-color" : {
+                    "baselineValue": "rgba(0, 0, 0, 0)",
+                    "triggerValue": "rgb(255, 255, 255)"
+                }
+            }
+        },
+        "ghostery" : {
+            "id": "ghostery-tracker-tally",
+            "clientHeight": 56,
+            "clientWidth": 160,
+            "offsetHeight": 56,
+            "offsetLeft": 758,
+            "offsetTop": 737,
+            "offsetWidth": 160,
+            "scrollHeight": 56,
+            "scrollWidth": 160
+        }
+        "darkreader" : {
+            "name": "darkreader",
+            "content": "e89e5c900c794250a2b4d324efde2bcf"
+        }
+    }
+},
+*/
--- public/fingerprint.js
+++ public/fingerprint.js
@@ -1,3 +1,11 @@
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
+
 var ext_trigger_id_mappings = {
     "bitwarden": ["31609","49960","49961","49962"],
     "adguardadblocker": ["28913","28914","28915","28916","28917","28918","28919","28920","28921","28922","28923","28924","28925","28926","28927"],
@@ -17,15 +25,14 @@ function detectedGCS(t, b) {
     let styleB = window.getComputedStyle(b);
 
     //diffPropertyValues is an array of JSON object CssMod that contain all style changes for this given trigger
-    var modifications = [];
+    var modifications = {};
 
     for (let property of styleT) {
         if (styleT.getPropertyValue(property) != styleB.getPropertyValue(property)) {
-            modifications.push({
-                "property": property,
+            modifications[property] = {
                 "baselineValue" : styleB.getPropertyValue(property),
                 "triggerValue" : styleT.getPropertyValue(property)
-            });
+            };
         }
     }
     // console.log(modifications);
@@ -34,14 +41,14 @@ function detectedGCS(t, b) {
 
 // var DOMextensions = []; //array of JSON object DomExt
 // var CSSextensions = []; //array of JSON object CssExt
-var extensions = [];
+var extensions = {};
 
 document.addEventListener("DOMContentLoaded", function(e) {
     /*
     CSS fingerprinting
     */
     var ts = document.getElementsByClassName('trigger');
-    var identifiedTriggers = []; //array of all identified trigger JSON object
+    var identifiedTriggers = {};
 
     // for all the triggers
     for (var i=0; i<ts.length; i++) {
@@ -51,49 +58,32 @@ document.addEventListener("DOMContentLoaded", function(e) {
         
         // get all the observed modifications for this trigger
         var modifications = detectedGCS(t, b);
-        if (modifications.length != 0) {
-            identifiedTriggers.push({
-                "tid" : tid,
-                "modifications" : modifications
-            });
+        if (!isEmpty(modifications)) {
+            identifiedTriggers[tid] = modifications;
         }
     }
-
-    // console.log(identifiedTriggers);
-
-    for (let eid in ext_trigger_id_mappings) {
-        extensions.push({
-            "eid" : eid,
-            "triggers" : []
-        });
-    };
 
     // now map this trigger to an extension
     for (let eid in ext_trigger_id_mappings) {
-        var ext;
-
-        for (var i=0; i<extensions.length; i++){
-            if (extensions[i]["eid"] == eid)
-                ext = extensions[i];
-        }
-        // console.log(ext);
         var possible_tids = ext_trigger_id_mappings[eid];
-        
-        for (var t=0; t<identifiedTriggers.length; t++){
-            var trigger = identifiedTriggers[t];
-            if (possible_tids.includes(trigger.tid)) {
-                ext["triggers"].push(trigger);
+        for (tid in identifiedTriggers){
+            if (possible_tids.includes(tid)) {
+                if (isEmpty(extensions[eid])) {
+                    extensions[eid] = {};
+                }
+                extensions[eid][tid] = identifiedTriggers[tid];
             }
         }
     }
+
+    // console.log(extensions); //excluding DOM
 
     /*
     DOM fingerprinting
     */
     let ghosteryElem = document.getElementById("ghostery-tracker-tally");
     if (ghosteryElem != null) {
-        var ghostery = {eid : "ghostery", properties : {}};
-        ghostery.properties = {
+        extensions["ghostery"] = {
             id: ghosteryElem.id,
             clientHeight : ghosteryElem.clientHeight,
             // clientLeft : ghosteryElem.clientLeft, //always 0
@@ -112,8 +102,6 @@ document.addEventListener("DOMContentLoaded", function(e) {
             // scrollTopMax : ghosteryElem.scrollTopMax,
             scrollWidth : ghosteryElem.scrollWidth
         };
-        // DOMextensions.push(ghostery);
-        extensions.push(ghostery);
     }
     else {
         console.log("Ghostery not detected");
@@ -122,21 +110,17 @@ document.addEventListener("DOMContentLoaded", function(e) {
     //Dark Reader meta tag
     let darkReaderMeta = document.querySelector('meta[name="darkreader"]')
     if (darkReaderMeta != null) {
-        var darkReader = {eid : "darkreader", properties : {}};
-        darkReader.properties = {
+        extensions["darkReader"] = {
             name: darkReaderMeta.name,
             content : darkReaderMeta.content
         };
-        // DOMextensions.push(darkReader);
-        extensions.push(darkReader);
     }
     else {
         console.log("Dark Reader not detected");
     }
 
-    // console.log(CSSextensions);
-    // console.log(DOMextensions);
-    // console.log(extensions);
+    // console.log(extensions); //including DOM
+
     var xhr = new XMLHttpRequest();
     var url = "http://localhost:3000/"; //change url based on ip_address
     xhr.open("POST", url, true);

bee8e79cf80e5effaa5e1463191add1d7eea4548
Brian Kim <hk2473@nyu.edu>
2021-12-05 04:00:58 +0400

    add different url for xml http request

--- public/fingerprint.js
+++ public/fingerprint.js
@@ -55,7 +55,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
         var tid = ts[i].getAttribute("id");
         var t = ts[i].querySelector("[trigger='yes']");
         var b = ts[i].querySelector("[trigger='no']");
-        
+
         // get all the observed modifications for this trigger
         var modifications = detectedGCS(t, b);
         if (!isEmpty(modifications)) {
@@ -132,8 +132,9 @@ document.addEventListener("DOMContentLoaded", function(e) {
     // console.log(extensions); //including DOM
 
     var xhr = new XMLHttpRequest();
-    // var url = "http://localhost:3000/"; //change url based on ip_address
-    var url = "http://10.21.54.204:3000/"; //change url based on ip_address
+    var url = "http://localhost:3000/"; //default localhost url
+    // var url = "http://10.21.54.204:3000/"; //laptop url
+    // var url = "http://10.230.240.222:3000/"; //server url
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
     var data = JSON.stringify(extensions);

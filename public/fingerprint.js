9ba8c55336c450f942134fe3e5184211b43c9169
Brian Kim <hk2473@nyu.edu>
2022-03-05 19:05:22 +0400

    midterm commit for lab server

--- public/fingerprint.js
+++ public/fingerprint.js
@@ -1,23 +1,11 @@
+var extensions = {};
+
 function isEmpty(obj) {
     for (var key in obj) {
         if (obj.hasOwnProperty(key))
             return false;
     }
     return true;
-}
-
-var ext_trigger_id_mappings = {
-    "bitwarden": ["31609","49960","49961","49962"],
-    "adguardadblocker": ["28913","28914","28915","28916","28917","28918","28919","28920","28921","28922","28923","28924","28925","28926","28927"],
-    "noscriptsecuritysuite": ["1","2","3"],
-    "leechblock": ["11","12","13","14","15"]
-};
-
-var eid_ptag_mappings = {
-    "bitwarden": "p4",
-    "adguardadblocker": "p5",
-    "noscriptsecuritysuite": "p6",
-    "leechblock": "p7"
 }
 
 function detectedGCS(t, b) {
@@ -35,8 +23,48 @@ function detectedGCS(t, b) {
             };
         }
     }
-    // console.log(modifications);
+
+    // if (!isEmpty(modifications))
+    //     console.log(modifications);
+
     return modifications;
+}
+
+function cssFP(){
+    //get the modified elements
+    var elems = document.getElementsByClassName('trigger');
+    var identifiedElems = {};
+    for (var i = 0; i < elems.length; i++) {
+        var elemid = elems[i].getAttribute("id");
+        var t = elems[i].querySelector("[trigger='yes']"); //trigger
+        var b = elems[i].querySelector("[trigger='no']"); //baseline
+
+        // get all the observed modifications for this element pair
+        var modifications = detectedGCS(t, b);
+        if (!isEmpty(modifications)) {
+            identifiedElems[elemid] = modifications;
+        }
+    }
+    // console.log(identifiedElems);
+
+    // now map the identifiedElems to their respective extensions
+    for (let extid in extid_to_elemid) { //extid is the key to extid_to_elemid json
+        
+        // possibly identified elements for every extension
+        var possibleElems = extid_to_elemid[extid];
+        // console.log(extid, possibleElems);
+
+        for (const elemid of possibleElems) { //pelemid is the element to possibleElems array
+            if (identifiedElems.hasOwnProperty(elemid)) { // actually identified
+                if (isEmpty(extensions[extid])) { // first identified element for extension
+                    extensions[extid] = {}; // create new key-object
+                }
+                extensions[extid][elemid] = identifiedElems[elemid]; // save info
+            }
+        }
+    }
+
+    // console.log("cssFP() done");
 }
 
 function getDimensionJSON(element) {
@@ -53,122 +81,58 @@ function getDimensionJSON(element) {
     };
 }
 
-var extensions = {};
-
-document.addEventListener("DOMContentLoaded", function(e) {
-    /*
-    CSS fingerprinting
-    */
-    var ts = document.getElementsByClassName('trigger');
-    var identifiedTriggers = {};
-
-    // for all the triggers
-    for (var i=0; i<ts.length; i++) {
-        var tid = ts[i].getAttribute("id");
-        var t = ts[i].querySelector("[trigger='yes']");
-        var b = ts[i].querySelector("[trigger='no']");
-
-        // get all the observed modifications for this trigger
-        var modifications = detectedGCS(t, b);
-        if (!isEmpty(modifications)) {
-            identifiedTriggers[tid] = modifications;
-        }
-    }
-
-    // now map this trigger to an extension
-    for (let eid in ext_trigger_id_mappings) {
-        var possible_tids = ext_trigger_id_mappings[eid];
-        for (tid in identifiedTriggers){
-            if (possible_tids.includes(tid)) {
-                if (isEmpty(extensions[eid])) {
-                    extensions[eid] = {};
-                }
-                extensions[eid][tid] = identifiedTriggers[tid];
-            }
-        }
-    }
-
-    // console.log(extensions); //excluding DOM
-
-    /*
-    DOM fingerprinting
-    */
-    let ghosteryElem = document.getElementById("ghostery-tracker-tally");
-    if (ghosteryElem != null) {
-        console.log(ghosteryElem);
+// function domFP(){
+//     let ghosteryElem = document.getElementById("ghostery-tracker-tally");
+//     if (ghosteryElem != null) {
+//         // console.log(ghosteryElem);
         
-        // dimensions
-        extensions["ghostery"] = getDimensionJSON(ghosteryElem);
-        
-        // extensions["ghostery"] = {
-        //     clientHeight : ghosteryElem.clientHeight,
-        //     clientLeft : ghosteryElem.clientLeft, //always 0
-        //     clientTop : ghosteryElem.clientTop, //always 0
-        //     clientWidth : ghosteryElem.clientWidth,
+//         // dimensions
+//         extensions["ghostery"] = getDimensionJSON(ghosteryElem);
 
-        //     offsetHeight : ghosteryElem.offsetHeight,
-        //     offsetLeft : ghosteryElem.offsetLeft,
-        //     offsetTop : ghosteryElem.offsetTop,
-        //     offsetWidth : ghosteryElem.offsetWidth,
+//         // other attributes to add - NamedNodeMap
+//         let attributes = ghosteryElem.attributes;
+//         // console.log(attributes);
+//         for (var i=0; i<attributes.length; i++){
+//             let item = attributes.item(i);
+//             extensions["ghostery"][item.name] = item.value;
+//         }
+//     }
+//     // else {
+//     //     console.log("Ghostery not detected");
+//     // }
 
-        //     scrollHeight : ghosteryElem.scrollHeight,
-        //     scrollLeft : ghosteryElem.scrollLeft, //always 0
-        //     scrollTop : ghosteryElem.scrollTop, //always 0
-        //     scrollWidth : ghosteryElem.scrollWidth
-        // };
+//     //Dark Reader meta tag
+//     let darkReaderMeta = document.querySelector('meta[name="darkreader"]')
+//     if (darkReaderMeta != null) {
+//         // console.log(darkReaderMeta);
 
-        // other attributes to add - NamedNodeMap
-        let attributes = ghosteryElem.attributes;
-        console.log(attributes);
-        for (var i=0; i<attributes.length; i++){
-            let item = attributes.item(i);
-            extensions["ghostery"][item.name] = item.value;
-        }
-    }
-    else {
-        console.log("Ghostery not detected");
-    }
+//         //dimensions - don't seem useful for DarkReader specifically but still good for initialization and consistency
+//         extensions["darkReader"] = getDimensionJSON(darkReaderMeta);
 
-    //Dark Reader meta tag
-    let darkReaderMeta = document.querySelector('meta[name="darkreader"]')
-    if (darkReaderMeta != null) {
-        console.log(darkReaderMeta);
+//         // other attributes to add - NamedNodeMap
+//         let attributes = darkReaderMeta.attributes;
+//         // console.log(attributes);
+//         for (var i=0; i<attributes.length; i++){
+//             let item = attributes.item(i);
+//             extensions["darkReader"][item.name] = item.value;
+//         }
+//     }
+//     // else {
+//     //     console.log("Dark Reader not detected");
+//     // }
+// }
 
-        //dimensions - don't seem useful but still good for initialization and consistency
-        extensions["darkReader"] = getDimensionJSON(darkReaderMeta);
+window.onload = function() {
+    cssFP();
+    // domFP();
 
-        // other attributes to add - NamedNodeMap
-        let attributes = darkReaderMeta.attributes;
-        console.log(attributes);
-        for (var i=0; i<attributes.length; i++){
-            let item = attributes.item(i);
-            extensions["darkReader"][item.name] = item.value;
-        }
-    }
-    else {
-        console.log("Dark Reader not detected");
-    }
-
-    // console.log(extensions); //including DOM
+    console.log(extensions);
 
     var xhr = new XMLHttpRequest();
-    var url = "http://localhost:3000/"; //default localhost url
-    // var url = "http://10.21.54.204:3000/"; //laptop url
-    // var url = "http://10.230.240.222:3000/"; //server url
+    // var url = "http://localhost:3000/"; //default localhost url
+    var url = "http://10.230.240.222:3000/"; //server url
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
     var data = JSON.stringify(extensions);
     xhr.send(data);
-
-    console.log(document.styleSheets.length);
-    for (var i=0; i<document.styleSheets.length; i++){
-        console.log(document.styleSheets[i]);
-    }
-});
-
-// window.onload = function(e){
-//     console.log(document.styleSheets.length);
-//     for (var i=0; i<document.styleSheets.length; i++){
-//         console.log(document.styleSheets[i].cssRules);
-//     }
-// }
+}

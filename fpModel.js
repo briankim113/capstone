09023027109ca5a73c36c25b0dafe8ab03f53a35
Brian Kim <hk2473@nyu.edu>
2022-03-15 14:21:41 +0400

    restructure Model and have both RECORDS and EXTENSIONS in the same JSON

--- fpModel.js
+++ fpModel.js
@@ -37,7 +37,7 @@ const mongoose = require('mongoose');
 
 const Fp = mongoose.Schema({
     date : {type : Date, default : Date.now},
-    extensions : Object
+    data : Object
 })
 
 const FpModel = mongoose.model('fps', Fp);
--- public/send.js
+++ public/send.js
@@ -1,12 +1,16 @@
 window.addEventListener('load', function() {
-    console.log(records); //from mutation.js
-    console.log(extensions); //from fingerprint.js
+    // console.log(records); //from mutation.js
+    // console.log(extensions); //from fingerprint.js
 
-    // var xhr = new XMLHttpRequest();
-    // var url = "http://localhost:3000/"; //default localhost url
-    // // var url = "http://10.230.240.222:3000/"; //server url
-    // xhr.open("POST", url, true);
-    // xhr.setRequestHeader("Content-Type", "application/json");
-    // var data = JSON.stringify(extensions);
-    // xhr.send(data);
+    var data = {};
+    data["RECORDS"] = records;
+    data["EXTENSIONS"] = extensions;
+    // console.log(data);
+
+    var xhr = new XMLHttpRequest();
+    var url = "http://localhost:3000/"; //default localhost url
+    // var url = "http://10.230.240.222:3000/"; //server url
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(data));
 });

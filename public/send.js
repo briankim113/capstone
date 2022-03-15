9a4138968a7b4e49cfade9bc4272a2bb11bcaf4f
Brian Kim <hk2473@nyu.edu>
2022-03-15 14:39:11 +0400

    change send url from localhost to lab server

--- public/send.js
+++ public/send.js
@@ -8,8 +8,8 @@ window.addEventListener('load', function() {
     // console.log(data);
 
     var xhr = new XMLHttpRequest();
-    var url = "http://localhost:3000/"; //default localhost url
-    // var url = "http://10.230.240.222:3000/"; //server url
+    // var url = "http://localhost:3000/"; //default localhost url
+    var url = "http://10.230.240.222:3000/"; //server url
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(JSON.stringify(data));
